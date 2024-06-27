28dccca6c0829e979f67f947951fc855454ac2c8
SupperGabor <gabor1415@gmail.com>
2024-06-27 20:47:09 +0200

    removed unnecessary comments moved morgan and nodemon to devdependencies created config/db

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,14 @@
-//Imports
+
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
-//initialize dotenv
+
 require("dotenv").config();
-//initialize database
-require("./config/mongoose.js");
-//Initializing app
+
+require("./config/db/mongoose.js");
 const app = express()
-//Middleware for html responses during development
+
 app.use(morgan("dev"));
 
 app.listen(3000, () => console.log("Server is running on port 3000"))
