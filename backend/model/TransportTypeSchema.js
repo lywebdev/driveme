8e9cce6b6cbe53b283dd7cad35f5432d5473bea5
SupperGabor <gabor1415@gmail.com>
2024-06-28 16:58:38 +0200

    Created transporttypeschema

---
+++ backend/model/TransportTypeSchema.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const TransportTypeSchema = new Schema({
+    id: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    name : {
+        type: String,
+        required: true
+    },
+    photo:{
+        type: String,
+        default: null
+    },
+    }
+
+)
+
+const TransportType = mongoose.model("TransportType", TransportTypeSchema);
+module.exports = TransportType;
