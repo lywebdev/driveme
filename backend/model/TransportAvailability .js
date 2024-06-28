0598f8c05febc679dc5283718bb8f1479230f1b9
SupperGabor <gabor1415@gmail.com>
2024-06-28 14:05:23 +0200

    fixed indentation

--- backend/model/TransportAvailability .js
+++ backend/model/TransportAvailability .js
@@ -1,26 +1,28 @@
-const mongoose = require ("mongoose");
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const TransportAvailabilitySchema = new Schema ({
+const TransportAvailabilitySchema = new Schema({
+  //TODO create TransportSchema to be referenced
+  transportId: {
+    type: Number,
+  },
 
-    //TODO create TransportSchema to be referenced
-    transportId: {
-        type: Number
+  date: {
+    type: Date,
+    required: true,
+  },
+  //TODO create slotsSchema to be referenced
+  slots: [
+    {
+      start: { type: Number, required: true },
+      end: { type: Number, required: true },
+      available: { type: Boolean, required: true },
     },
+  ],
+});
 
-    date: {
-        type: Date,
-        required: true
-    },
-    //TODO create slotsSchema to be referenced
-    slots:
-    [{
-    start: { type: Number, required: true },
-    end: { type: Number, required: true },
-    available: { type: Boolean, required: true }
-    }]
-
-})
-
-const TransportAvailability = mongoose.model("TransportAvailability", TransportAvailabilitySchema);
-module.exports = TransportAvailability
+const TransportAvailability = mongoose.model(
+  "TransportAvailability",
+  TransportAvailabilitySchema
+);
+module.exports = TransportAvailability;
