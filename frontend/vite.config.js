804c65fc5075af03cd7988ffa257d76a92429be6
Kulik Leonid <lyonyauei@gmail.com>
2024-06-30 17:44:19 +0200

    DMF-3: upgrade README.md

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import eslintPlugin from 'vite-plugin-eslint';
-import legacy from '@vitejs/plugin-legacy'
+import legacy from '@vitejs/plugin-legacy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,4 +16,14 @@ export default defineConfig({
       targets: ['defaults', 'not IE 11'],
     }),
   ],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @import "src/sass/_fonts.scss";
+          @import "src/sass/_variables.scss";
+        `,
+      },
+    },
+  },
 });
