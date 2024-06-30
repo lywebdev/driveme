cfe1df3b2438bb5eb7f28e8d4a092d69dce95711
Kulik Leonid <lyonyauei@gmail.com>
2024-06-30 18:36:18 +0200

    DMB-7: Creating a SlotSchema

---
+++ backend/models/SlotSchema.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const SlotSchema = new Schema({
+  start: {
+    type: Number,
+    required: true,
+  },
+  end: {
+    type: Number,
+    required: true,
+  },
+  available: {
+    type: Boolean,
+    required: true,
+  },
+});
+
+
+SlotSchema.pre("save", function(next) {
+    if(this.start >= this.end) {
+        next(new Error("Start time must be before end time"));
+    } else if(this.end - this.start < 60) {
+        next(new Error("Slot must be at least one hour long"));
+    } else {
+        next()
+    }
+})
+
+const Slot = mongoose.model("Slot", SlotSchema);
+
+export default Slot
