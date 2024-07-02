19e73aee99763c810ce37616212a61e33c08f177
Kulik Leonid <lyonyauei@gmail.com>
2024-07-02 21:51:12 +0200

    DMB-10: creating a user schema

---
+++ backend/models/UserSchema.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+import moment from "moment";
+import UserConstants from "./constants/UserConstants.js";
+
+const { Schema } = mongoose;
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            min: 3,
+            max: 20,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+            minlength: 6,
+        },
+        role: {
+            type: String,
+            enum: [
+                UserConstants.ROLE_ADMIN,
+                UserConstants.ROLE_USER,
+            ],
+            default: UserConstants.ROLE_USER,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: function (created_at) {
+                return moment(created_at).format("YYYY-MM-DD HH:mm:ss");
+            },
+        },
+    },
+);
+
+const User = mongoose.model("User", UserSchema);
+
+export default User;
---
+++ backend/models/constants/UserConstants.js
@@ -0,0 +1,7 @@
+const UserConstants = {
+    ROLE_ADMIN: 'admin',
+    ROLE_USER: 'user',
+};
+
+
+export default UserConstants;
