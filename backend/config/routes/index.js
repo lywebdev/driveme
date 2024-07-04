1c567c618126f2e7a35e0738aaeedec333281703
SupperGabor <gabor1415@gmail.com>
2024-07-04 17:53:50 +0200

    DMB-12 fixed issues

--- backend/config/routes/index.js
+++ backend/config/routes/index.js
@@ -1,8 +1,8 @@
 import express from "express";
-import userRoutes from "./userRoutes.js";
+import userRoutes from "./usersRoutes.js";
 
 const router = express.Router();
 
 router.use("/users", userRoutes);
 
-export default router
+export default router;
