f9bc5fdea02580cf7fddb28335535b864a971bd2
Kulik Leonid <lyonyauei@gmail.com>
2024-07-13 00:05:59 +0200

    DMB-17: add handleRequest function and simplify transportType controller

--- backend/controller/transportTypeController.js
+++ backend/controller/transportTypeController.js
@@ -1,33 +1,14 @@
 import {findAll, findByIdAndUpdate, removeById, store} from "../services/transportTypeService.js";
+import {handleRequest} from "../services/baseService.js";
 
-const all = async (req, res) => {
-  const responseData = await findAll();
+const all = (req, res) => handleRequest(req, res, findAll);
 
-  res.status(responseData.status)
-      .json(responseData.content);
-};
+const create = (req, res) => handleRequest(req, res, store, req.body);
 
-const create = async (req, res) => {
-  const responseData = await store(req.body);
+const remove = (req, res) => handleRequest(req, res, removeById, req.params.id);
 
-  res.status(responseData.status)
-      .json(responseData.content);
-};
+const update = (req, res) => handleRequest(req, res, findByIdAndUpdate, req.params.id, req.body);
 
-const remove = async (req, res) => {
-  const { id } = req.params;
-  const responseData = await removeById(id);
 
-  res.status(responseData.status)
-      .json(responseData.content);
-};
-
-const update = async (req, res) => {
-  const { id } = req.params;
-  const responseData = await findByIdAndUpdate(id, req.body);
-
-  res.status(responseData.status)
-      .json(responseData.content);
-};
 
 export default { all, create, remove, update };
