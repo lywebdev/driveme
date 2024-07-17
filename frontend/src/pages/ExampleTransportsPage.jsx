793b16dc373a90ad961658c423599ceeafb570ab
SupperGabor <gabor1415@gmail.com>
2024-07-17 10:40:42 +0200

    DMF-7 created custom hook for pagination

--- frontend/src/pages/ExampleTransportsPage.jsx
+++ frontend/src/pages/ExampleTransportsPage.jsx
@@ -7,25 +7,22 @@ import VehicleCard from "@components/shared/Vehicle/VehicleCard";
 import { useState } from 'react';
 import "./ExampleTransportsPage.css";
 import Dropdown from 'react-dropdown';
+import usePagination from "../hooks/usePagination";
 
 const ExampleTransportsPage = () => {
     const [transports] = useExampleTransportsStore((state) => [state.transports]);
-    const [currentPage, setCurrentPage] = useState(1);
     const [vehicleTypes, setVehicleTypes] = useState("All");
     const [sorting, setSorting] = useState("None");
+
+    const { totalPages, handlePageClick, currentTransports} = usePagination(transports, 8);
     
-    const cardsPerPage = 8;
-    const startIndex = (currentPage - 1) * cardsPerPage;
-    const endIndex = startIndex + cardsPerPage;
-    const currentTransports = transports.slice(startIndex, endIndex);
-    const totalPages = Math.ceil(transports.length / cardsPerPage);
     const ArrowClosed = () => <span>&#9660;</span>;
     const ArrowOpen = () => <span>&#9650;</span>;
     const vehicleTypeOptions = [
-        { value: "All", label: "All" },
-        { value: "Car", label: "Car" },
-        { value: "Scooter", label: "Scooter" },
-        { value: "Bike", label: "Bike" },
+        { value: "0", label: "All" },
+        { value: "1", label: "Car" },
+        { value: "2", label: "Scooter" },
+        { value: "3", label: "Bike" },
     ];
 
     const sortingOptions = [
@@ -35,11 +32,6 @@ const ExampleTransportsPage = () => {
         { value: "Rating", label: "Rating" },
 
     ];
-
-    const handlePageClick = (pageIndex) => {
-        console.log("Changing to page:", pageIndex + 1);
-        setCurrentPage(pageIndex + 1);
-    };
 
 
     const handleVehicleTypeChange = (selectedOption) => {
@@ -59,6 +51,7 @@ const ExampleTransportsPage = () => {
             <PageTitle className="page-title">
                 <PageTitle.Top className="text-centered top">
                 Rent a vehicle near you!
+
                 </PageTitle.Top>
             </PageTitle>
 
