90dd40d845edbf06408ea20dbd05baedc50037c4
SupperGabor <gabor1415@gmail.com>
2024-07-22 17:25:24 +0200

    DMF-9 Created loginpage.

--- frontend/src/pages/Auth/LoginPage.jsx
+++ frontend/src/pages/Auth/LoginPage.jsx
@@ -1,13 +1,15 @@
 import Container from "@layouts/shared/Container.jsx";
 import PageTitle from "@components/shared/PageTitle/PageTitle.jsx";
 import AuthForm from "@components/features/AuthForm/AuthForm.jsx";
+import  '../styles/LoginPage.scss';
 
 const Login = () => {
-    return <div className='login-page'>
+    return <div className="login-page">
         <Container>
             <PageTitle className='page-title'>
                 <PageTitle.Top className='text-centered top'>Login</PageTitle.Top>
             </PageTitle>
+            <PageTitle.Text className='text-centered'>Welcome back! Please log in using the form to rent a vehicle</PageTitle.Text>
             <AuthForm />
         </Container>
     </div>;
--- frontend/src/routes/routes.jsx
+++ frontend/src/routes/routes.jsx
@@ -1,7 +1,7 @@
 import Dashboard from "@pages/admin/Dashboard.jsx";
 import HomePage from "@pages/HomePage.jsx";
 import {routes} from "@config/routes.js";
-import Login from "@pages/Auth/Login.jsx";
+import LoginPage from "@pages/Auth/LoginPage.jsx";
 import TransportsPage from "@pages/TransportsPage.jsx";
 
 export const publicRoutes = [
@@ -18,7 +18,7 @@ export const publicRoutes = [
 export const guestRoutes = [
     {
         path: routes.login,
-        element: <Login />,
+        element: <LoginPage />,
     }
 ];
 
