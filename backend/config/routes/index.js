207f30b5afe12764069bdc9a50f81faacd29b454
SupperGabor <gabor1415@gmail.com>
2024-07-23 19:02:07 +0200

    DMB-16 Created transports route

--- backend/config/routes/index.js
+++ backend/config/routes/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import usersRoutes from "./usersRoutes.js";
 import transportTypeRoutes from "./transportTypeRoutes.js";
+import transportsRoutes from "./transportsRoutes.js";
 
 const router = express.Router();
 
 router.use("/users", usersRoutes);
 router.use("/transport-types", transportTypeRoutes);
+router.use("/transports", transportsRoutes)
 
 export default router;
---
+++ backend/controller/transportController.js
@@ -0,0 +1,23 @@
+import TransportService from "../services/transportService.js";
+
+const all = async (req, res) => {
+    const response = await TransportService.findAll();
+    res.status(response.status).json(response.content);
+}
+
+const create = async (req, res) => {
+    const response = await TransportService.store(req.body);
+    res.status(response.status).json(response.content);
+}
+
+const remove = async (req, res) => {
+    const response = await TransportService.removeById(req.params.id);
+    res.status(response.status).json(response.content);
+}
+
+const update = async (req, res) => {
+    const response = await TransportService.findByIdAndUpdate(req.params.id, req.body);
+    res.status(response.status).json(response.content);
+}
+
+export default { all, create, remove, update }
